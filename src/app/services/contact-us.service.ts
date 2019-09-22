c1bc2419b220ba650f418a93daa345059c9272ab
Akshay Raut <rakshay08@gmail.com>
2019-09-23 04:15:53 +0530

    Added AlertifyJS for success and error messages

--- src/app/services/contact-us.service.ts
+++ src/app/services/contact-us.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { environment } from 'src/environments/environment';
+import * as alertify from 'alertifyjs'
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,17 @@ import { environment } from 'src/environments/environment';
 export class ContactUsService {
   contactUsApiurl = "/ContactUs/SendEmail";
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) { 
+    alertify.defaults.closable = false;
+  }
 
   sendMessage(name: string, email: string, subject: string, message: string) {
     this.http.post(environment.mphlixApiUrl + this.contactUsApiurl, { name: name, email: email, subject: subject, message: message })
     .subscribe(response => {
-      console.log(response);
+      alertify.success('Thank you. Our skin specialist will contact you as soon as possible.');
+    }, error => {
+      alertify.error('Oops! Looks like an error occurred. Please try again.');
+      console.log(error);
     });
   }
 }
