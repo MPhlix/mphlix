dbdd92835da3094eeb677e4811d8298b4ad53382
Akshay Raut <rakshay08@gmail.com>
2020-04-12 00:51:35 +0530

    Working - Angular animation transition height 1. transition on changing questions

--- src/app/components/skin-type-quiz/skin-type-quiz.component.ts
+++ src/app/components/skin-type-quiz/skin-type-quiz.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Question } from 'src/app/interface/Question';
 import { SkinTypeService } from 'src/app/services/skin-type/skin-type.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'skin-type-quiz',
   templateUrl: './skin-type-quiz.component.html',
-  styleUrls: ['./skin-type-quiz.component.css']
+  styleUrls: ['./skin-type-quiz.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: "70px" }),
+            animate('500ms ease-in')
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class SkinTypeQuizComponent implements OnInit {
   questions: Question[];
