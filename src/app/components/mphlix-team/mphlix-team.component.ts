31a54acf1eb5b2c4cd2ad6498803b1955b835d3c
Akshay Raut <rakshay08@gmail.com>
2020-04-18 21:01:52 +0530

    Removed screen resize detection code Code is working but currently it's not required

--- src/app/components/mphlix-team/mphlix-team.component.ts
+++ src/app/components/mphlix-team/mphlix-team.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, HostListener, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/interface/Member';
 import { MembersService } from 'src/app/services/members.service';
-import { SCREEN_SIZE } from 'src/app/enums/screen-size.enum';
-import { ScreenResizeService } from 'src/app/services/screen-resize.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
@@ -10,7 +8,7 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   templateUrl: './mphlix-team.component.html',
   styleUrls: ['./mphlix-team.component.css']
 })
-export class MphlixTeamComponent implements OnInit, AfterViewInit {
+export class MphlixTeamComponent implements OnInit {
 
   socialMediaUrl = [
     { name: 'instagram', url: 'https://www.instagram.com/' },
@@ -20,15 +18,6 @@ export class MphlixTeamComponent implements OnInit, AfterViewInit {
   ];
 
   members: Member[] = [];
-
-  prefix = 'is-';
-  sizes = [
-    { id: SCREEN_SIZE.XS, name: 'xs', css: `d-block d-sm-none` },
-    { id: SCREEN_SIZE.SM, name: 'sm', css: `d-none d-sm-block d-md-none` },
-    { id: SCREEN_SIZE.MD, name: 'md', css: `d-none d-md-block d-lg-none` },
-    { id: SCREEN_SIZE.LG, name: 'lg', css: `d-none d-lg-block d-xl-none` },
-    { id: SCREEN_SIZE.XL, name: 'xl', css: `d-none d-xl-block` },
-  ];
 
   customOptions: OwlOptions = {
     loop: true,
@@ -57,42 +46,14 @@ export class MphlixTeamComponent implements OnInit, AfterViewInit {
     autoplayHoverPause: true
   }
 
-  constructor(private membersService: MembersService, private screenResizeService: ScreenResizeService,
-    private elementRef: ElementRef) {
+  constructor(private membersService: MembersService) {
     this.members = this.membersService.getAllQuestions();
   }
 
   ngOnInit() {
   }
 
-  ngAfterViewInit() {
-    this.detectScreenSize();
-  }
-
   getSocialMediaProfile(socialMedia: string, profileId: string) {
     return this.socialMediaUrl.find(a => a.name === socialMedia).url + profileId;
-  }
-
-  @HostListener("window:resize", [])
-  private onResize() {
-    this.detectScreenSize();
-  }
-
-  private detectScreenSize() {
-    const currentSize = this.sizes.find(x => {
-      // console.log('vale of x', x.name);
-      // if(x.name === 'xs'){
-      //   this.config.slidesPerView = 1;
-      // }
-      // get the HTML element
-      const el = this.elementRef.nativeElement.querySelector(`.${this.prefix}${x.id}`);
-
-      // check its display property value
-      const isVisible = window.getComputedStyle(el).display != 'none';
-
-      return isVisible;
-    });
-
-    this.screenResizeService.onResize(currentSize.id);
   }
 }
