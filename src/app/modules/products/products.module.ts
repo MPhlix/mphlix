eca67fd1a060222b774988d505999fb07516e1a9
Akshay Raut <rakshay08@gmail.com>
2020-04-27 01:23:57 +0530

    Added banner image for Search Products Moved products components to their main components folder

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './products/products.component';
-import { SearchProductComponent } from './search-product/search-product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SearchProductComponent } from './components/search-product/search-product.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
