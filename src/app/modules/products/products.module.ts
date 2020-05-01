f2efac2af0b7e9727ef74d3a88ddd1b628017a6a
Akshay Raut <rakshay08@gmail.com>
2020-05-01 20:19:38 +0530

    #6 - Show products listing on Products page First draft ready

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './components/products/products.component';
 import { SearchProductComponent } from './components/search-product/search-product.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListingComponent } from './components/products-listing/products-listing.component';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductsComponent }
@@ -11,7 +12,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     ProductsComponent,
-    SearchProductComponent
+    SearchProductComponent,
+    ProductsListingComponent
   ],
   imports: [
     CommonModule,
